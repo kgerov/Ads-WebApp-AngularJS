278bbad081b15e3d2115c23b3a3b2ba285e44484
kgerov <kgerov@abv.bg>
2014-12-28 23:04:21 +0200

    config app + routing

--- app/app.js
+++ app/app.js
@@ -1,5 +1,11 @@
 var adsApp = angular.module('adsApp', ['ngRoute']);
 
 adsApp.config(function($routeProvider) {
-	
+	$routeProvider
+		.when('/',
+		{
+			controller: '',
+			templateUrl: 'app/partials/home.html'
+		})
+		.otherwise(redirectTo: '/');
 });
