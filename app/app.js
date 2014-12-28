74a50caefabb19eb2fd2d5e3d95f1271f71cc857
kgerov <kgerov@abv.bg>
2014-12-28 23:32:32 +0200

    PNoty + changes in index

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
 var adsApp = angular.module('adsApp', ['ngRoute']);
 
-adsApp.config(function($routeProvider) {
-	$routeProvider
-		.when('/',
-		{
-			controller: '',
-			templateUrl: 'app/partials/home.html'
-		})
-		.otherwise(redirectTo: '/');
-});
+// adsApp.config(function($routeProvider) {
+// 	// $routeProvider
+// 	// 	.when('/',
+// 	// 	{
+// 	// 		controller: '',
+// 	// 		templateUrl: 'app/partials/home.html'
+// 	// 	})
+// 	// 	.otherwise(redirectTo: '/');
+// });
