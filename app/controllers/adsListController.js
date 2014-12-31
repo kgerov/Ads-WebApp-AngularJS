28e42ee14ae1063de2febadb34991b053f7517dd
kgerov <kgerov@abv.bg>
2014-12-31 17:36:48 +0200

    working filters

--- app/controllers/adsListController.js
+++ app/controllers/adsListController.js
@@ -1,19 +1,31 @@
 adsApp.controller('adsListController', ['$scope', 'adsFactory', 'pageSize', function($scope, adsFactory, pageSize) {
-	$scope.ads = [];
+	$scope.filterAds = { categoryFilterRadio : 'all-cat', townFilterRadio : 'all-town' };
+
+    $scope.ads = [];
     $scope.adsCount = 0;
     $scope.usersPerPage = pageSize;
     getPageContent(1);
 
-  	$scope.pageChangeHandler = function(num) {
-    	getPageContent(num);
+    $scope.filterChange = function () {
+        $scope.pageChangeHandler(1, $scope.filterAds);
+        //$('#goToPageOne').triggerHandler('on-page-change');
+    }
+
+  	$scope.pageChangeHandler = function(num, filters) {
+        var cat = filters.categoryFilterRadio,
+            town = filters.townFilterRadio,
+            catId = cat != 'all-cat' ? cat.substring(1, cat.length) : '',
+            townId = town != 'all-town' ? town.substring(1, town.length) : '';
+            
+    	getPageContent(num, townId, catId);
   	};
 
-  	function getPageContent (pageNumber) {
-  		adsFactory.getAdsFromPage(pageNumber).$promise
+  	function getPageContent (pageNumber, townId, catId) {
+  		adsFactory.getAdsFromPage(pageNumber, townId, catId).$promise
 			.then(function (data) {
 				$scope.ads = data.ads;
 				$scope.adsCount = data.numItems;
-                          $('html, body').animate({scrollTop : 0},100);
+        $('html, body').animate({scrollTop : 0},100);
 			}, function (error) {
 
 			});
--- app/factories/adsFactory.js
+++ app/factories/adsFactory.js
@@ -1,9 +1,9 @@
 adsApp.factory('adsFactory', ['$resource', '$http', 'pageSize', function($resource, $http, pageSize){
-	var resource = $resource('http://softuni-ads.azurewebsites.net/api/ads?pagesize=:size&startpage=:pageNum',
-		{size: '@size', pageNum: '@pageNum'});
+	var resource = $resource('http://softuni-ads.azurewebsites.net/api/ads?pagesize=:size&startpage=:pageNum&townid=:cityid&categoryid=:catid',
+		{size: '@size', pageNum: '@pageNum', cityid: '@cityid', catid: '@catid'});
 
-	function getAdsFromPage (desiredPage) {
-		return resource.get({ size: pageSize, pageNum: desiredPage });
+	function getAdsFromPage (desiredPage, townId, catId) {
+		return resource.get({ size: pageSize, pageNum: desiredPage, cityid: townId, catid: catId });
 	}
 
 	return {
