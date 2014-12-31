1eaa9aa31b69ac7cdb51deab4d7a8416ab30f866
kgerov <kgerov@abv.bg>
2014-12-31 17:38:51 +0200

    adsFactory updated + changes in pagination plugin + new todos

--- app/controllers/adsListController.js
+++ app/controllers/adsListController.js
@@ -33,3 +33,4 @@ adsApp.controller('adsListController', ['$scope', 'adsFactory', 'pageSize', func
 }]);
 
 //https://cdn3.iconfinder.com/data/icons/free-3d-glossy-interface-icon-set/64/Erase.png
+//a
