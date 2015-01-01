754466f2927dfbd7e0a80bfa4215402289e7281b
kgerov <kgerov@abv.bg>
2015-01-01 15:47:29 +0200

    file upload with image preview with styling

--- app/controllers/newAdController.js
+++ app/controllers/newAdController.js
@@ -1,53 +1,49 @@
-adsApp.controller('newAdController', ['$scope', 'FileUploader', function($scope, FileUploader){
-	 var uploader = $scope.uploader = new FileUploader({
-	            url: 'upload.php'
-	 });
+adsApp.controller('newAdController', ['$scope', 'categoryFactory', 'townFactory', '$http', function($scope, categoryFactory, townFactory, $http) {
+    $scope.categories = [];
+    $scope.towns = [];
 
-        // FILTERS
+    init();
 
-        uploader.filters.push({
-            name: 'imageFilter',
-            fn: function(item /*{File|FileLikeObject}*/, options) {
-                var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
-                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+    function init () {
+        categoryFactory.getAllCategories().$promise
+            .then(function (data) {
+                $scope.categories = data;
+            }, function (error) {
+                
+            });
+
+        townFactory.getAllTowns().$promise
+            .then(function (data) {
+                $scope.towns = data;
+            }, function (error) {
+                
+            });
+    }
+
+    $(":file").filestyle({buttonText: "Browse...", buttonName: "btn-primary"});
+
+	 function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            
+            reader.onload = function (e) {
+                $('#preview-pic').attr('src', e.target.result);
             }
-        });
+            
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+    $("#ad-pic-upload").change(function() {
+        readURL(this);
+        $('#remove-img').css('visibility', 'visible');
+        $('#remove-img').css('display', 'inline-block');
+    });
 
-        // CALLBACKS
-
-        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
-            console.info('onWhenAddingFileFailed', item, filter, options);
-        };
-        uploader.onAfterAddingFile = function(fileItem) {
-            console.info('onAfterAddingFile', fileItem);
-        };
-        uploader.onAfterAddingAll = function(addedFileItems) {
-            console.info('onAfterAddingAll', addedFileItems);
-        };
-        uploader.onBeforeUploadItem = function(item) {
-            console.info('onBeforeUploadItem', item);
-        };
-        uploader.onProgressItem = function(fileItem, progress) {
-            console.info('onProgressItem', fileItem, progress);
-        };
-        uploader.onProgressAll = function(progress) {
-            console.info('onProgressAll', progress);
-        };
-        uploader.onSuccessItem = function(fileItem, response, status, headers) {
-            console.info('onSuccessItem', fileItem, response, status, headers);
-        };
-        uploader.onErrorItem = function(fileItem, response, status, headers) {
-            console.info('onErrorItem', fileItem, response, status, headers);
-        };
-        uploader.onCancelItem = function(fileItem, response, status, headers) {
-            console.info('onCancelItem', fileItem, response, status, headers);
-        };
-        uploader.onCompleteItem = function(fileItem, response, status, headers) {
-            console.info('onCompleteItem', fileItem, response, status, headers);
-        };
-        uploader.onCompleteAll = function() {
-            console.info('onCompleteAll');
-        };
-
-        console.info('uploader', uploader);
+    $('#remove-img').on('click', function () {
+        $(":file").filestyle('clear');
+        $('#preview-pic').attr('src', 'http://img4.wikia.nocookie.net/__cb20130819001030/lego/images/a/ac/No-Image-Basic.png');
+        $('#remove-img').css('visibility', 'hidden');
+        $('#remove-img').css('display', 'none');
+    });
 }])
