4745f4b1da1d2198edaf1783be3c1ad4004cb2a0
kgerov <kgerov@abv.bg>
2015-01-03 17:27:47 +0200

    design made more responsive

--- app/filters/validImg.js
+++ app/filters/validImg.js
@@ -6,4 +6,13 @@ angular.module('validImgFilter', []).filter('validImg', function ($q) {
 			return 'https://cdn0.iconfinder.com/data/icons/very-basic-android-l-lollipop-icon-pack/24/cancel-2-128.png';
 		}
 	}
+})
+.filter('emptyText', function ($q) {
+	return function (text) {
+		if (text) {
+			return text;
+		} else {
+			return 'No Information';
+		}
+	}
 });
