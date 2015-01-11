c889720e0257a6365b4efa561d3a1f3f7c3abc3a
kgerov <kgerov@abv.bg>
2015-01-11 02:20:37 +0200

    edit user profile + set pass in service

--- app/factories/userFactory.js
+++ app/factories/userFactory.js
@@ -2,19 +2,38 @@ adsApp.factory('userFactory', ['$resource', '$http', 'baseUrl', 'userPageSize', 
 	var resource = $resource(baseUrl + 'admin/users?sortBy=:sort&pagesize=:size&startpage=:page',
 		{sort: '@sort', size: '@size', page: '@page'});
 
-	var deleteResource = $resource(baseUrl + 'admin/user/:user',
-		{user: '@user'});
+	var userResource = $resource(baseUrl + 'admin/user/:user',
+		{user: '@user'}, { 
+		update: {
+			method: 'PUT'
+		}});
+
+	var setPassResource = $resource(baseUrl + 'admin/setpassword',
+		{}, { 
+		update: {
+			method: 'PUT'
+		}});
 
 	function getAllUsers (pageNum, filter) {
 		return resource.get({sort: filter, size: userPageSize, page: pageNum});
 	}
 
+	function editUser(username, user) {
+		return userResource.update({user: username}, user);
+	}
+
+	function setPassword(newPass) {
+		return setPassResource.update(newPass);
+	}
+
 	function deleteUser (username) {
-		return deleteResource.delete({user: username});
+		return userResource.delete({user: username});
 	}
 
 	return {
 		getAllUsers: getAllUsers,
-		deleteUser: deleteUser
+		deleteUser: deleteUser,
+		editUser: editUser,
+		setPassword: setPassword
 	}
 }])
